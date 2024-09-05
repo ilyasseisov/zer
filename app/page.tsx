f2b258a254472880df80a2a76f4972e1bb1cef8e
Ilyas Seisov <ilyasseisov@gmail.com>
2024-09-05 12:01:52 +0500

    #148::general project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import Link from 'next/link';
+import { FC } from 'react';
+const Home: FC = () => {
+  return (
+    <main>
+      <h1 className='text-3xl font-stolzl font-normal '>Home</h1>
+      <h1 className='text-3xl font-stolzl font-medium'>Home</h1>
+      <h1 className='text-3xl font-stolzl font-bold'>Home</h1>
+    </main>
+  );
+};
 
-export default function Home() {
-  return <main></main>;
-}
+export default Home;
