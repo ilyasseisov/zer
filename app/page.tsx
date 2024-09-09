fea1a1ab982f9e42ef7bb1f0f8ea3ab790d73adb
Ilyas Seisov <ilyasseisov@gmail.com>
2024-09-09 11:37:57 +0500

    #160: create <Button>

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+// FC
 import { FC } from 'react';
+// components
+import Hero from '@/components/Hero';
 const Home: FC = () => {
   return (
-    <main>
-      <h1 className='text-3xl font-stolzl font-normal '>Home</h1>
-      <h1 className='text-3xl font-stolzl font-medium'>Home</h1>
-      <h1 className='text-3xl font-stolzl font-bold'>Home</h1>
+    <main className='container w-full max-w-none'>
+      <Hero />
     </main>
   );
 };
