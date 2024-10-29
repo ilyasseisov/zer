47bd6feba9234edf563791a9c05634dd35589d1b
Ilyas Seisov <ilyasseisov@gmail.com>
2024-10-29 14:15:11 +0500

    #231:: fix cursor position onScroll

--- utils/useMousePosition.tsx
+++ utils/useMousePosition.tsx
@@ -8,23 +8,32 @@ type mouseCoordinates = {
   y: number | null;
 };
 
-const useMousePosition = (): mouseCoordinates => {
+const useMousePosition = (): mouseCoordinates & { scrollY: number } => {
   // hooks
   const [mousePosition, setMousePosition] = useState<mouseCoordinates>({
     x: null,
     y: null,
   });
+  const [scrollY, setScrollY] = useState<number>(0);
 
   const updateMousePosition = (e: MouseEvent) => {
     setMousePosition({ x: e.clientX, y: e.clientY });
   };
 
+  const updateScrollPosition = () => {
+    setScrollY(window.scrollY);
+  };
+
   useEffect(() => {
     window.addEventListener("mousemove", updateMousePosition);
+    window.addEventListener("scroll", updateScrollPosition);
 
-    return () => window.removeEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+      window.removeEventListener("scroll", updateScrollPosition);
+    };
   }, []);
 
-  return mousePosition;
+  return { x: mousePosition.x, y: mousePosition.y, scrollY };
 };
 export default useMousePosition;
