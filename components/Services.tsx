8c3ce5a080574c419ab11b32daf17a7f93497e6d
Ilyas Seisov <ilyasseisov@gmail.com>
2024-12-23 15:37:25 +0500

    fix small issues

--- components/Services.tsx
+++ components/Services.tsx
@@ -12,7 +12,7 @@ type HoverProps = {
 
 const Services: FC<HoverProps> = ({ setIsHoveredSize0 }) => {
   return (
-    <section className="relative grid min-h-screen grid-cols-[repeat(auto-fit,_8.333333%)] justify-center md:px-0">
+    <section className="relative grid min-h-screen grid-cols-[repeat(auto-fit,_8.333333%)] justify-center overflow-x-hidden md:px-0">
       <div className="col-span-12 flex w-full flex-col items-start justify-center text-white">
         <h3 className="text-20px-capsized md:text-32px-capsized mb-8 w-full pl-4 font-stolzl font-normal text-grey md:mb-16 md:pl-[8.333333%] xl:pl-[16.666666%]">
           WHAT WE DO
